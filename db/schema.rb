6868659bafcfb0818dbed896da8f07df6bfb8d96
Farah288 <farahizzati.99@gmail.com>
2021-10-25 08:43:46 +0100

    changes to Gem file and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_040536) do
+ActiveRecord::Schema.define(version: 2021_10_25_023637) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "picture"
+    # t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
